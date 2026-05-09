71e9da12cee7e8b0830dc5451dfeebee3178145a
redblackgaming <redblackgaming@users.noreply.github.com>
2026-05-09 14:17:55 +0000

    deploy: redblackgaming/metagross-calculator@03c066b9f9d11959ddc32d239282508ae448d7b9

--- _next/static/Jy1j6lNSIffTp_bWCtHtu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/240-29c2a4cebdef74b1.js","static/chunks/pages/index-2317b080f3c1c505.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
